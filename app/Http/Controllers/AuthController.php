b47f6c5f7ba580b086e9bd3922c107e39293c298
levskey <levboss13@hotmail.com>
2026-03-24 02:27:22 +0100

    Ninja Network week 6

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -33,9 +34,22 @@ class AuthController extends Controller
         return redirect()->route('ninjas.index');
     }
 
-    public function Login ()
+    public function Login (Request $request)
     {
-        
+        $validated = $request->validate([
+          'email' => 'required|email',
+          'password' => 'required|string'
+        ]);
+
+        if (Auth::attempt($validated)){
+            $request->session()->regenerate();
+
+            return redirect()->route('ninjas.index');
+        } 
+
+         throw ValidationException::withMessages([
+                'credentials' => 'Sorry, incorrect credentials'
+            ]);
     }
 
     public function logout (Request $request)
